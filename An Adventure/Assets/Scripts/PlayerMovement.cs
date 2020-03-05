8c13930eed9bf24df97eac1640a8aa6cce3a678e
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-03-05 09:20:52 +0200

    SM: Added basic rotation and cleaned the code.

--- An Adventure/Assets/Scripts/PlayerMovement.cs
+++ An Adventure/Assets/Scripts/PlayerMovement.cs
@@ -11,31 +11,35 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 2.0f;
     public float Force = 2.0f;
     public bool isGrounded;
+    public int dashCooldown = 3;
+    public int rotated = 0;
 
     void Start()
-    {   
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.transform.Rotate(0, -90, 0, Space.Self);
+        rotated--;
         jump = new Vector3(0.0f, 2.0f, 0.0f);
         leftDash = new Vector3(-2.0f, 0.0f, 0.0f);
-        rightDash = new Vector3(2.0f, 0.0f, 0.0f);
+        InvokeRepeating("DecreaseCooldown", 1.0f, 1.0f);
     }
 
     void Update()
-    {   
+    {
         Rigidbody rb = GetComponent<Rigidbody>();
         float horizontalAxis = Input.GetAxis("Horizontal");
         Vector3 displacement = new Vector3(horizontalAxis, 0, 0) * Time.deltaTime * PlayerSpeed;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-        } else if (Input.GetKeyDown(KeyCode.LeftShift) && horizontalAxis<0)
+            Jump(rb);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            rb.AddForce(leftDash * Force, ForceMode.Impulse);
-        } else if (Input.GetKeyDown(KeyCode.LeftShift) && horizontalAxis > 0)
-        {
-            rb.AddForce(rightDash * Force, ForceMode.Impulse);
-        } 
+            Dash(horizontalAxis, rb);
+        }
 
+        Rotate(rb);
         rb.MovePosition(transform.position + displacement);
     }
 
@@ -47,6 +51,49 @@ public class PlayerMovement : MonoBehaviour
     void OnCollisionStay()
     {
         isGrounded = true;
+    }
+
+    void DecreaseCooldown()
+    {
+        if (dashCooldown > 0)
+        {
+            dashCooldown--;
+        }
+    }
+
+    void Dash(float horizontalAxis, Rigidbody rb)
+    {
+        if (horizontalAxis < 0 && dashCooldown == 0)
+        {
+            rb.AddForce(leftDash * Force, ForceMode.Impulse);
+            dashCooldown = 3;
+        }
+        if (horizontalAxis > 0 && dashCooldown == 0)
+        {
+            rb.AddForce(leftDash * -1 * Force, ForceMode.Impulse);
+            dashCooldown = 3;
+        }
+    }
+
+    void Jump(Rigidbody rb)
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void Rotate(Rigidbody rb)
+    {
+        if(Input.GetKeyDown(KeyCode.A) && rotated < 0)
+        {
+            rb.transform.Rotate(0, 180, 0, Space.Self);
+            rotated += 2;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && rotated > 0) { 
+            rb.transform.Rotate(0, -180, 0, Space.Self);
+            rotated -= 2;
+        }
     }
 }
 
