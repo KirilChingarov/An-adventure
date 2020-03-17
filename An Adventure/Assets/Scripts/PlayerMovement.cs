707b3e8e272381e97c89cafbab5e7868cc036e73
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-03-17 18:57:36 +0200

    SM: Added stable version of Das ability. A bug was found where if the directions were switched faster than 50 ms the rotation breaks and rotats not as expected.

--- An Adventure/Assets/Scripts/PlayerMovement.cs
+++ An Adventure/Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float PlayerSpeed = 0.1f;
-    public Vector3 jump;
-    public Vector3 leftDash;
-    public Vector3 rightDash;
+    public float playerSpeed = 0.1f;
     public float jumpForce = 2.0f;
-    public int isGrounded = 1;
     public int dashCooldown = 3;
-    public float dashSpeed = 2;
+    public float dashSpeed = 2.0f;
+    public float startDashTime;
     private int rotated = 0;
+    private int isGrounded = 1;
     private float horizontalAxis;
     private Rigidbody rb;
+    private int direction;
+    private float dashTime;
+    private bool dashed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.transform.Rotate(0, -90, 0, Space.Self);
         rotated--;
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
-        leftDash = new Vector3(-2.0f, 0.0f, 0.0f);
         InvokeRepeating("DecreaseCooldown", 1.0f, 1.0f);
+        dashTime = startDashTime;
     }
 
     void Update()
     {
-        horizontalAxis = Input.GetAxis("Horizontal");
-        Vector3 displacement = new Vector3(horizontalAxis, 0, 0) * Time.deltaTime * PlayerSpeed;
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown == 0)
         {
             Dash();
         }
@@ -41,8 +38,77 @@ public class PlayerMovement : MonoBehaviour
             Jump();
         }
 
+        if (dashTime <= 0 && dashed)
+        {
+            direction = 0;
+            rb.velocity = Vector3.zero;
+            dashed = false;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+
+            if (direction == 1)
+            {
+                rb.velocity = Vector3.left * dashSpeed;
+            }
+            else if (direction == 2)
+            {
+                rb.velocity = Vector3.right * dashSpeed;
+            }
+        }
+
+        horizontalAxis = Input.GetAxis("Horizontal");
+        Vector3 displacement = new Vector3(horizontalAxis, 0, 0) * Time.deltaTime * playerSpeed;
         rb.MovePosition(transform.position + displacement);
         Rotate();
+    }
+
+
+    void Dash()
+    {
+        dashed = true;
+        dashCooldown = 3;
+        dashTime = startDashTime;
+
+        if (horizontalAxis < 0)
+        {
+            direction = 1;
+        }
+        if (horizontalAxis > 0)
+        {
+            direction = 2;
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.Impulse);
+    }
+
+    void Rotate()
+    {    
+        if (Input.GetKey(KeyCode.A) && rotated < 0 && horizontalAxis < 0)
+        {
+            StartCoroutine(RotateMe(Vector3.up * 180, 0.05f));
+            rotated = 1;
+        }
+        if(Input.GetKey(KeyCode.D) && rotated > 0 && horizontalAxis > 0) {
+            StartCoroutine(RotateMe(Vector3.up * -180, 0.05f));
+            rotated = -1;
+        }
+    }
+
+    IEnumerator RotateMe(Vector3 angles, float seconds)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + angles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / seconds)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
     }
 
     void OnCollisionExit()
@@ -61,59 +127,6 @@ public class PlayerMovement : MonoBehaviour
         {
             dashCooldown--;
         }
-    }
-
-    void Dash()
-    {
-        //rb.velocity = transform.forward * dashSpeed;
-        if (horizontalAxis < 0 && dashCooldown == 0)
-        {
-            rightDash *= Time.deltaTime * dashSpeed;
-            rb.MovePosition(transform.position + rightDash);
-            dashCooldown = 3;
-        }
-        if (horizontalAxis > 0 && dashCooldown == 0)
-        {
-            leftDash *= Time.deltaTime * dashSpeed;
-            rb.MovePosition(transform.position + leftDash);
-            dashCooldown = 3;
-        }
-    }
-
-    void Jump()
-    { 
-        rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-    }
-
-    void Rotate()
-    {    
-        if (Input.GetKey(KeyCode.A) && rotated < 0 && horizontalAxis < 0)
-        {
-            StartCoroutine(RotateMe(Vector3.up * 180, 0.05f));
-            rotated = 1;
-        }
-        if(Input.GetKey(KeyCode.D) && rotated > 0 && horizontalAxis > 0) {
-            StartCoroutine(RotateMe(Vector3.up * -180, 0.05f));
-            rotated = -1;
-        }
-    }
-
-    private IEnumerator Delay()
-    {
-        yield return new WaitForSeconds(0.25f);
-        rb.useGravity = true;
-    }
-
-    IEnumerator RotateMe(Vector3 angles, float seconds)
-    {
-        var fromAngle = transform.rotation;
-        var toAngle = Quaternion.Euler(transform.eulerAngles + angles);
-        for (var t = 0f; t < 1; t += Time.deltaTime / seconds)
-        {
-            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
-            yield return null;
-        }
-        transform.rotation = toAngle;
     }
 }
 
