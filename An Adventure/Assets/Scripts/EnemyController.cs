3e341602bc875a1eeb70b203e06cda34a511d67b
KirilChingarov <kiril.chingarov@gmail.com>
2020-03-21 14:40:37 +0200

    Added Health to enemy and damage to fireball

---
+++ An Adventure/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health = 100;
+
+    public void TakeDamgae(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log(name + " died");
+        Destroy(gameObject);
+    }
+}
--- An Adventure/Assets/Scripts/FireBallController.cs
+++ An Adventure/Assets/Scripts/FireBallController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FireBallController : MonoBehaviour
 {
     public float fireBallSpeed = 1.0f;
+    public int fireBallDamage = 50;
     public Rigidbody rb;
 
     void Update()
@@ -12,8 +13,16 @@ public class FireBallController : MonoBehaviour
         rb.velocity = transform.forward * fireBallSpeed;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider hitInfo)
     {
+        Debug.Log(hitInfo.name);
+
+        EnemyController enemy = hitInfo.GetComponent<EnemyController>();
+        if(enemy != null)
+        {
+            enemy.TakeDamgae(fireBallDamage);
+        }
+        
         Destroy(gameObject);
     }
 }
