5df48042bcc023c23db62641347875456ad035a1
KirilChingarov <kiril.chingarov@gmail.com>
2020-03-21 13:54:53 +0200

    Added Combat test scene and fireball ability

---
+++ An Adventure/Assets/Scripts/FireBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject projectile;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(projectile, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ An Adventure/Assets/Scripts/FireBallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : MonoBehaviour
+{
+    public float fireBallSpeed = 1.0f;
+    public Rigidbody rb;
+
+    void Update()
+    {
+        rb.velocity = transform.forward * fireBallSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
