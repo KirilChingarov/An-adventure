86485c55cf49d603497f7b898220cccca2219ae7
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-04-06 13:45:51 +0300

    SD: Removed some commented code.

--- An Adventure/Assets/Scripts/FPSLimiter.cs
+++ An Adventure/Assets/Scripts/FPSLimiter.cs
@@ -21,19 +21,4 @@ public class FPSLimiter : MonoBehaviour
 		}
 		dt += (Time.unscaledDeltaTime - dt);
 	}
-
-/*	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 25;
-		style.normal.textColor = new Color(1, 1, 1, 1f);
-		float msec = dt * 1000f;
-		float fps = 1f / dt;
-		string fpsText = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, fpsText, style);
-	}*/
 }
--- An Adventure/Assets/Scripts/GameEnding.cs
+++ An Adventure/Assets/Scripts/GameEnding.cs
@@ -16,7 +16,6 @@ public class GameEnding : MonoBehaviour
         if(player.position.x >= 65 || player.position.y <= -5)
         {
             Debug.Log("Game Over!");
-            //UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
         }
     }
