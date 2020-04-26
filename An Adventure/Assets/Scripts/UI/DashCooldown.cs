32c960e9a4ff7003a22401c110f26ecd7f6c9432
KirilChingarov <kiril.chingarov@gmail.com>
2020-04-26 15:32:28 +0300

    Added dash cooldown bar

---
+++ An Adventure/Assets/Scripts/UI/DashCooldown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashCooldown : MonoBehaviour
+{
+    private HealthBar slider;
+    public Dash dash;
+
+    private void Start()
+    {
+        slider = GetComponent<HealthBar>();
+    }
+
+    void Update()
+    {
+        slider.setHealth(3 - dash.dashCooldown);
+    }
+}
